4e68b25f71090bf22bd73e1b308dff2fa8d95f92
Francis Adeboye <adeboye.francis@icloud.com>
2025-04-27 20:52:14 +0100

    updated tfoutput for sensitive values

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,14 +6,17 @@ output "webapp_public_ip" {
 output "vpc_id" {
   description = "ID of the VPC"
   value       = data.tfe_outputs.infra-connection.values.main_vpc
+  sensitive = true
 }
 
 output "public_subnets" {
   description = "List of IDs of the public subnets"
   value       = data.tfe_outputs.infra-connection.values.public_subnets
+  sensitive = true
 }
 
 output "private_subnets" {
   description = "List of IDs of the private subnets"
   value       = data.tfe_outputs.infra-connection.values.private_subnets
+  sensitive = true
 }
